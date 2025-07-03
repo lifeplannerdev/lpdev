2e12a9e15cdb7ded3408e3bb0517ad8c96a8d240
unknown <rishebs123456@gmail.com>
2025-07-03 18:34:05 +0530

    added new university logos

--- models.py
+++
@@ -1,9 +0,0 @@
-class TeamMember(models.Model):
-    name = models.CharField(max_length=100)
-    post = models.CharField(max_length=100)
-    img = models.ImageField(upload_to='pics')
-    bio = models.TextField()
-    phone = models.CharField(max_length=15, blank=True, null=True)
-
-    def __str__(self):
-        return self.name 
